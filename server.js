1f3387796eabaf25d6ea7229915eb0c4efed09cc
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2023-10-22 22:17:34 +0530

    Completed

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+
+
+const PORT = process.env.PORT || 3000
+
+http.listen(PORT, () => {
+    console.log(`Listening On port ${PORT}`);
+});
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) =>{
+    res.sendFile(__dirname + '/index.html')
+});
+
+// socket
+
+const io = require('socket.io')(http)
+
+
+io.on('connection', (socket) =>{
+    console.log('Connection...');
+    socket.on('message', (msg) =>{
+        socket.broadcast.emit('message', msg)
+    })
+});
